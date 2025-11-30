61bdb54bfd8549c083dd44074c6c1a4a0283593e
Mohammed Fackruddin Tusher <fackruddin.tusher@gmail.com>
2025-11-30 14:19:06 +0600

    feat(main): Create client

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-model='gemini-2.0-flash-001', contents="Why is the sky blue?"
-)
-print(response.text)
+
+
+def main():
+        load_dotenv()
+        api_key = os.environ.get("GEMINI_API_KEY")
+        client = genai.Client(api_key=api_key)
+        if len(sys.argv) < 2:
+            print("Please provide a prompt as a command-line argument.")
+            sys.exit(1)
+        prompt = sys.argv[1]    
+        response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=prompt
+        )
+        print(response.text)
+        if response.usage_metadata is not None:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        else:
+            print("Usage metadata is not available in the response.")
+main()
