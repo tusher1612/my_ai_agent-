b7e08a6f22cff98b984b4289900608b9b184b710
Mohammed Fackruddin Tusher <fackruddin.tusher@gmail.com>
2025-11-30 15:00:30 +0600

    feat(main): Implement calculator setup

--- main.py
+++ main.py
@@ -2,23 +2,34 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
-        load_dotenv()
-        api_key = os.environ.get("GEMINI_API_KEY")
-        client = genai.Client(api_key=api_key)
-        if len(sys.argv) < 2:
-            print("Please provide a prompt as a command-line argument.")
-            sys.exit(1)
-        prompt = sys.argv[1]    
-        response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=prompt
-        )
-        print(response.text)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    if len(sys.argv) < 2:
+        print("Please provide a prompt as a command-line argument.")
+        sys.exit(1)
+    verbose_flag=False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+            verbose_flag=True
+            sys.exit(1)    
+    prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", contents=messages
+    )
+    print(response.text)
+    if verbose_flag:
         if response.usage_metadata is not None:
             print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
             print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
         else:
             print("Usage metadata is not available in the response.")
+
+
 main()
